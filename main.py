275d5fa4d0816ac1eb50d8a581134d173928f657
shankhalam <shankhalamh@gmail.com>
2023-05-18 22:10:36 +0530

    send content to mail.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import runpy
+
 import requests
+from send_mail import send_mail
 
 api_key = "3bc3669be1304f0a80375d5776034dc4"
 url = "https://newsapi.org/v2/top-headlines?country=us&" \
@@ -8,6 +11,11 @@ url = "https://newsapi.org/v2/top-headlines?country=us&" \
 request = requests.get(url)
 content = request.json()
 
+body = ""
 for article in content["articles"]:
-      print(article["title"])
-      print(article["content"])
+    if article["description"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode('utf-8')
+send_mail(message=body)
+
