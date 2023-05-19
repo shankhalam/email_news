09e0be683a6eb153a33bd7d1e0dea478716773cc
shankhalam <shankhalamh@gmail.com>
2023-05-19 21:48:54 +0530

    Appearance change and subject add.

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-import runpy
-
+# Import Libraries
 import requests
 from send_mail import send_mail
 
+# API key from site
 api_key = "3bc3669be1304f0a80375d5776034dc4"
-url = "https://newsapi.org/v2/top-headlines?country=us&" \
-      "category=business&apiKey=" \
-      "3bc3669be1304f0a80375d5776034dc4"
+url = "https://newsapi.org/v2/top-headlines?" \
+      "country=us&" \
+      "category=business&" \
+      "apiKey=""3bc3669be1304f0a80375d5776034dc4&" \
+      "language=en"
 
 request = requests.get(url)
 content = request.json()
 
+# Body of message(email)
 body = ""
-for article in content["articles"]:
+for article in content["articles"][0:20]:
     if article["description"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = "Subject: Today's News" \
+               + "\n" + body + article["title"] + "\n" \
+               + article["description"] + "\n" \
+               + article["url"] + 2*"\n"
 
 body = body.encode('utf-8')
 send_mail(message=body)
